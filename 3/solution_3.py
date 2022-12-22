f57e7c2dede2e8a5aa28d0fc855a229ce8a57b57
James Langley <jwblangley@gmail.com>
2022-12-22 10:56:51 +0000

    Write get_intersection

--- 3/solution_3.py
+++ 3/solution_3.py
@@ -1,4 +1,5 @@
 from collections import Counter
 
 def get_intersection(a: str, b: str) -> Counter[str]:
-    pass
+    # More efficient would be to stop counting as soon as we get one match
+    return Counter(a) & Counter(b)
