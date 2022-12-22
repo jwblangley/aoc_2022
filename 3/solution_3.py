0326324acea67ba53ff480d5189d54d562a0ef76
James Langley <jwblangley@gmail.com>
2022-12-22 11:26:10 +0000

    Implement day 3 part 1 main

--- 3/solution_3.py
+++ 3/solution_3.py
@@ -14,3 +14,22 @@ def get_priority(l: str) -> int:
         return ord(l) - ord("a") + 1
     else:
         return ord(l) - ord("A") + 27
+
+
+def counter_to_char(c: Counter[str]) -> str:
+    assert len(c) == 1, "There should only be one intersecting key"
+    return next(c.elements())
+
+
+def split_line(line: str) -> tuple[str, str]:
+    assert len(line) % 2 == 0, "Only even splits are allowed"
+    return line[: len(line) // 2], line[len(line) // 2 :]
+
+
+if __name__ == "__main__":
+    with open("3/input.txt") as f:
+        sum_priorities = sum(
+            get_priority(counter_to_char(get_intersection(*split_line(line.strip()))))
+            for line in f
+        )
+        print(f"Sum of priorities: {sum_priorities}")
