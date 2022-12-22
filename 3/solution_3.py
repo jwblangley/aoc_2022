d3d582f78bf0716f3d52f7db8c8b36768663bad3
James Langley <jwblangley@gmail.com>
2022-12-22 11:14:40 +0000

    Write test_get_priority

--- 3/solution_3.py
+++ 3/solution_3.py
@@ -3,3 +3,6 @@ from collections import Counter
 def get_intersection(a: str, b: str) -> Counter[str]:
     # More efficient would be to stop counting as soon as we get one match
     return Counter(a) & Counter(b)
+
+def get_priority(l: str) -> int:
+    pass
