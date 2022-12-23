c2be7c87a87ddcfc59c27c441b82c296bad1bf21
James Langley <jwblangley@gmail.com>
2022-12-23 10:32:55 +0000

    Implement main for day 4 part 1

--- 4/solution_4.py
+++ 4/solution_4.py
@@ -16,3 +16,19 @@ def is_proper_subset_range(*rs: Range) -> bool:
 
     # Need to check equality since there may be shared boundaries
     return any(r == Range(minimin, maximax) for r in rs)
+
+
+def line_to_ranges(line: str) -> tuple[Range, Range]:
+    match [r.split("-") for r in line.strip().split(",")]:
+        case [[ll, lu], [rl, ru]]:
+            return Range(int(ll), int(lu)), Range(int(rl), int(ru))
+        case _:
+            raise ValueError(f'Could not parse line: "{line}"')
+
+
+if __name__ == "__main__":
+    with open("4/input.txt") as f:
+        num_total_overlaps = sum(
+            1 for line in f if is_proper_subset_range(*line_to_ranges(line))
+        )
+    print(f"Num total overlaps: {num_total_overlaps}")
